60643204180e29473030c40702cf9564887f8e24
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-07-22 14:00:02 -0500

    Funciones

---
+++ JavaScript-U/8.Funciones.js
@@ -0,0 +1,7 @@
+//Definicion de funcoines
+//Una funciion es un bloque de codigo reutilizabla, que realiza una tarea en particular
+
+//Una funcion puede tener parametros de entrada (opcionales) y devuelve un valor de salida
+
+//Sintaxis de una funcion
+// function <nombre de la funcion>(<parametros>){cuerpo de la funcion  return<valor retornado>}fin
