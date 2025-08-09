11ac85191d1f788a58fb9507862824442d3bd9bc
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-08-08 20:35:02 -0500

    Retornar el nuev valor

--- JavaScript-U/9.PasoValor.js
+++ JavaScript-U/9.PasoValor.js
@@ -14,6 +14,16 @@ let numero = 5
 modificarValor(numero)
 console.log(numero)
 
+//Funciona exactamente igual con el return porque estamos retornando un nuevo valor, no modificando el original.
 
+function modificarValor1(valor){
+    valor=21
+    console.log(valor) 
+    // Si queremos ver el valor modificado fuera de la funcion,
+    // debemos retornar el nuevo valor y asignarlo a la variable original.
+    return valor
+}
 
-    
+let numero1 = 5
+numero1 = modificarValor1 (numero1)
+console.log(numero1)
