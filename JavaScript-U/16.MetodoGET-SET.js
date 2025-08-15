9f558d0cd48dbb620f518fb86085f65b23369eb2
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-08-15 11:18:54 -0500

    Metodo SET

--- JavaScript-U/16.MetodoGET-SET.js
+++ JavaScript-U/16.MetodoGET-SET.js
@@ -21,3 +21,56 @@ console.log(`Hola mi nombre es ${persona.nombre} tengo ${persona.edad} años y s
 console.log(persona.nombreCompleto)
 
 //METODO SET EN JS --------------------------------------------------------------------------
+//Este metodo nos permite modificar la propiedad o atributo de un objeto
+
+let persona1 = {
+    nombre: "Mario",
+    edad: 29,
+    profesion: "Desarrollador",
+    idioma: "es",
+    get lang(){
+        return this.idioma.toUpperCase()
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase()
+    },
+    get nombreCompleto(){
+        return this.nombre + " " + this.edad
+    }
+}
+
+console.log(persona1.lang) //Muestra el idioma en mayusculas
+persona1.lang = "en" //Cabiamos el idioma al ingles
+console.log(persona1.lang) //Muestra el idioma en mayusculas
+console.log(persona1.idioma)
+
+function suma(a, b){
+    let resultado =a+b
+    console.log(resultado)
+}
+console.log(suma(8,9))
+
+
+//NOTAS ----------------------------------------------------------------------------
+// 2️⃣ En el caso del get y set
+// get lang() {
+//     return this.idioma.toUpperCase();
+// },
+// set lang(lang) {
+//     this.idioma = lang.toUpperCase();
+// }
+
+
+// El getter (get lang) es como una propiedad calculada: siempre devuelve algo (en tu caso, this.idioma.toUpperCase()), así que nunca será undefined salvo que el valor interno lo sea.
+
+// El setter (set lang) no devuelve nada (su propósito es asignar), por eso cuando lo usas:
+// persona1.lang = "en"
+// no ves nada, pero internamente cambia la propiedad idioma.
+
+// Resumen:
+
+// Función sin return → devuelve undefined.
+
+// Getter → devuelve lo que pongas en el return del getter.
+
+// Setter → no devuelve nada; su trabajo es modificar el valor.
