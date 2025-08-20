d422af421d56a595bb487b40410cde40a4d82257
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-08-20 12:46:31 -0500

    Diagonales de una matriz

--- JavaScript-U/7.Matrices.js
+++ JavaScript-U/7.Matrices.js
@@ -65,3 +65,75 @@ for(let i=0; i<inventario.length; i++){
 let matriz2 = [[100,200,300],[400,500]]
 
 console.table(matriz2)
+
+
+//FORMAS DE ACCEDER A LAS DIAGONALES DE UNA MATRIZ ---------------------------------------------------
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+    let diagonalD =0
+    let diagonalI =0
+   
+    for(let i=0; i<arr.length;i++){
+        for(let j=0; j<arr[i].length;j++){
+            if(i==j){
+                diagonalD += arr[i][j] 
+            }
+            if(i+j==arr[i].length-1){
+                diagonalI += arr[i][j]
+            }
+        }
+    }
+    let result = Math.abs(diagonalD-diagonalI)
+    return result
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
