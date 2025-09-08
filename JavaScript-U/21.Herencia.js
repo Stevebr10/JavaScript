d0c88bd3552d56d7c62236ff56f1c87093e929bb
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-09-08 15:48:01 -0500

    Hrencia

---
+++ JavaScript-U/21.Herencia.js
@@ -0,0 +1,49 @@
+//HERENCIA 
+//Es la capacidad que tiene una clase de heredar atributos y metodos de otra clase padre
+//La clase que herada se llama clase hija o subclase
+//La clase que hereda se llama clase padre o superclase
+//La herencia se implementa con la palabra reservada extends
+
+class Persona {
+    constructor(nombre, apellido, edad){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+    }
+    get getNombre(){
+        return this.nombre
+    }
+    get getApellido(){
+        return this.apellido
+    }
+    get getEdad(){
+        return this.edad
+    }
+    set setNombre(nombre){
+        return this.nombre = nombre
+    }
+    set setApellido(apellido){
+        return this.apellido = apellido
+    }
+    set setEdad(edad){
+        return this.edad = edad
+    }
+}
+
+class Empleado extends Persona {
+    constructor(nombre, apellido, edad, departamento){
+        super(nombre, apellido, edad) //LLama al constructor de la clase padre
+        this.departamento = departamento
+    }
+    get getDepartamento(){
+        return this.departamento
+    }
+    set setDepartamento(departamento){
+        return this.departamento = departamento
+    }
+}
+let empleado1 = new Empleado('Luis', 'Gomez', 28, 'Sistemas')
+console.log(empleado1.getNombre)
+console.log(empleado1.getApellido)
+console.log(empleado1.getEdad)
+console.log(empleado1.getDepartamento)
