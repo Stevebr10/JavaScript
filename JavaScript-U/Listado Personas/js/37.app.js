a9a6de972f257eb3eda6c6124f36628e5d5fc533
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-09-22 15:51:07 -0500

    Listado de Personas

--- JavaScript-U/Listado Personas/js/37.app.js
+++ JavaScript-U/Listado Personas/js/37.app.js
@@ -1,1 +1,18 @@
 console.log("Aplicacion Listado de Personas")
+
+
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Karla', 'Lara'),
+    new Persona('Carlos', 'Ramirez')
+]
+
+function mostrarPersonas(){
+    console.log("Mostrando Personas...")
+    let texto = ""
+    for(let persona of personas){
+        //texto += `Nombre: ${persona.getNombre}, Apellido: ${persona.getApellido}\n`
+        console.log(persona)
+    }
+    //console.log(texto)
+}
--- JavaScript-U/Listado Personas/js/38.persona.js
+++ JavaScript-U/Listado Personas/js/38.persona.js
@@ -1,1 +1,20 @@
-console.log("Clase Persona")
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre
+        this.apellido = apellido
+    }
+    get getNombre(){
+        return this.nombre
+    }
+    get getApellido(){
+        return this.apellido
+    }
+    set setNombre(nombre){
+        return this.nombre = nombre
+    }
+    set setApellido(apellido){
+        return this.apellido = apellido
+    }
+}
+
+console.log("Archivo persona.js cargado")
