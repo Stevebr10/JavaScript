7b3f7f7185e3d2cc47628864281ccff763ac488e
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-09-23 15:17:49 -0500

    Creacion de Arreglos Ingreso y Egresos

---
+++ JavaScript-U/Aplicacion Presupuesto/js/44.app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('Salario', 2100.00),
+    new Ingreso('Venta coche', 1500.00)
+
+]
+const egresos = [
+    new Egreso('Renta departamento', 900.00),
+    new Egreso('Ropa', 400.00)
+]
+
+let cargarApp = () => {  // Funcion que se ejecuta al iniciar la pagina el cual llama a otras funciones
+    cargarCabecero()
+
+}
+let cargarCabecero = () => { // Funcion que carga el cabecero de la pagina con los datos del presupuesto
+
+}
