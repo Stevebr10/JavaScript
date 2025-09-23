50ad6789d1029dd77e97178a9561e6834630bac6
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-09-23 15:32:43 -0500

    Cabecero

--- JavaScript-U/Aplicacion Presupuesto/js/44.app.js
+++ JavaScript-U/Aplicacion Presupuesto/js/44.app.js
@@ -12,6 +12,27 @@ let cargarApp = () => {  // Funcion que se ejecuta al iniciar la pagina el cual 
     cargarCabecero()
 
 }
-let cargarCabecero = () => { // Funcion que carga el cabecero de la pagina con los datos del presupuesto
 
+let totalIngresos = () => { // Funcion que calcula el total de ingresos
+    let totalIngreso = 0
+    for ( let ingreso of ingresos){
+        totalIngreso += ingreso.getValor //No se ocupa el parentesis porque es un metodo get 
+    }
+    return totalIngreso
+}
+ let totalEgresos = () => { // Funcion que calcula el total de egresos
+    let totalEgreso = 0
+    for( let egreso of egresos){
+        totalEgreso += egreso.getValor //Si se ocupa el parentesis entonces es un metodo normal 
+    }
+    return totalEgreso
+ }
+
+let cargarCabecero = () => { // Funcion que carga el cabecero de la pagina con los datos del presupuesto
+    let presupuesto = totalIngresos() - totalEgresos()
+    let porcentajeEgreso = totalEgresos() / totalIngresos()
+    document.getElementById('presupuesto').innerHTML = presupuesto
+    document.getElementById('porcentaje').innerHTML = porcentajeEgreso
+    document.getElementById('ingresos').innerHTML = totalIngresos()
+    document.getElementById('egresos').innerHTML = totalEgresos()
 }
